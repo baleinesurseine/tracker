f6b4ca714eb6a599d87ca651f5d4ac9ef99f987d
Edouard <edouard.fischer@gmail.com>
2016-09-06 20:51:26 +0200

    minor change

--- TimeSeries.js
+++ TimeSeries.js
@@ -1,4 +1,4 @@
-function TimeSeries(client, namespace) {
+function TimeSeries (client, namespace) {
   this.namespace = namespace
   this.client = client
   this.units = {
@@ -8,18 +8,18 @@ function TimeSeries(client, namespace) {
     day: 60 * 60 * 24
   }
   this.granularities = {
-    '1sec' : { name: '1sec', ttl: this.units.hour * 2, duration: this.units.second, quantity: this.units.minute * 5 },
-    '1min' : { name: '1min', ttl: this.units.day * 7, duration: this.units.minute, quantity: this.units.hour * 8 },
-    '1hour' : { name: '1hour', ttl: this.units.day * 60, duration: this.units.hour, quantity: this.units.day * 10 },
+    '1sec': { name: '1sec', ttl: this.units.hour * 2, duration: this.units.second, quantity: this.units.minute * 5 },
+    '1min': { name: '1min', ttl: this.units.day * 7, duration: this.units.minute, quantity: this.units.hour * 8 },
+    '1hour': { name: '1hour', ttl: this.units.day * 60, duration: this.units.hour, quantity: this.units.day * 10 },
     '1day': { name: '1day', ttl: null, duration: this.units.day, quantity: this.units.day * 30 }
   }
 }
-TimeSeries.prototype.insert = function(timeStampInSeconds, id) {
+TimeSeries.prototype.insert = function (timeStampInSeconds, id) {
   for (var granularitiyName in this.granularities) {
     var granularity = this.granularities[granularitiyName]
     var key = this._getKeyName(granularity, timeStampInSeconds, id)
     var fieldName = this._getRoundedTimeStamp(timeStampInSeconds, granularity.duration)
-    this.client.hincryby(key, fieldName, 1)
+    this.client.hincrby(key, fieldName, 1)
     if (granularity.ttl !== null) {
       this.client.expire(key, granularity.ttl)
     }
